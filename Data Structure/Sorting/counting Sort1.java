4901bd724e4547dfccae8923affd97ac3481d69b
Ved Prakash Yadav <vpyadav8961@gmail.com>
2018-01-13 22:45:19 +0530

    Counting Sort problem solved

---
+++ Data Structure/Sorting/counting Sort1.java
@@ -0,0 +1,26 @@
+//Link for this problem:: https://www.hackerrank.com/challenges/countingsort1/problem
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int SIZE = 100;
+        int[] ar = new int[SIZE];
+        for(int i = 0; i < SIZE; ar[i++] = 0){
+            
+        }
+        for(int N = in.nextInt(); N-- > 0; ar[in.nextInt()]++){
+            
+        }
+        for(int i = 0; i < SIZE; System.out.print(ar[i] + (++i < SIZE ? " ": ""))){
+            
+        }
+
+        in.close();
+    }
+}
