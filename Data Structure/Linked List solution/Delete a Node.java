4cbe9720fd4e982b4112414ee34a3727969bb56d
Ved Prakash Yadav <vpyadav8961@gmail.com>
2018-01-13 12:03:28 +0530

    delete a node solved

---
+++ Data Structure/Linked List solution/Delete a Node.java
@@ -0,0 +1,26 @@
+//Link for problem:: https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem
+/*
+  Delete Node at a given position in a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    // This is a "method-only" submission. 
+    // You only need to complete this method. 
+
+Node Delete(Node head, int position) {
+  // Complete this method
+Node node = head;
+    if(position == 0){
+        return node.next;
+    }
+    while(--position > 0){
+        node = node.next;
+    }
+    node.next = node.next.next;
+    return head;
+}
+
